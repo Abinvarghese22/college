598513dd85ac3d23608b4772ce2f39e83e0ad2fd
Abin varghese <159890336+Abinvarghese22@users.noreply.github.com>
2024-03-19 15:47:11 +0530

    Add files via upload

---
+++ MCA/2nd semester/OOPS_LAB_JAVA_MCA/others/JAVA-main/StartString.java
@@ -0,0 +1,9 @@
+class StartString {
+public static void main(String[]args) {
+String s1 = "Good Morning";
+System.out.println(s1);
+
+System.out.println("To uppercase:"+s1.toUpperCase());
+System.out.println("To lowercase:"+s1.toLowerCase());
+}
+}
